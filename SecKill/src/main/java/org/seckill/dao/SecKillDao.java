7a3027303cdac96df996763ce18165833ac41ab3
黄磊 <759482173@qq.com>
2019-08-02 01:20:27 +0800

    commit again

--- SecKill/src/main/java/org/seckill/dao/SecKillDao.java
+++ SecKill/src/main/java/org/seckill/dao/SecKillDao.java
@@ -1,5 +1,17 @@
 package org.seckill.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.seckill.entity.*;
+
 public interface SecKillDao {
 
+
+    int reduceNumber(long seckillid, Date killTime);
+
+    SecKill queryById(long seckillid);
+
+
+    List<SecKill> queryAll(int offset, int limit);
 }
--- SecKill/src/main/java/org/seckill/dao/SuccessKillDao.java
+++ SecKill/src/main/java/org/seckill/dao/SuccessKillDao.java
@@ -1,4 +1,11 @@
 package org.seckill.dao;
+import org.seckill.entity.*;
 
 public interface SuccessKillDao {
+    /*
+    *插入购买明细，可以过滤重复
+    * */
+    int insertSuccessKilled(long seckillid,long userPhone);
+
+    SuccessKilled queryByIdWithSeckill(long seckillId);
 }
